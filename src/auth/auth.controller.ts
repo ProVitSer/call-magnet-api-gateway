0598cc972eaddb559ac39af63ef37b7de760de4e
ProVitSer <prokinvs@gmail.com>
2023-10-22 21:13:51 +0000

    feat: add verify auth user route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterUserDto } from './dto/register-user.dto';
 import { Observable } from 'rxjs';
+import { VerifyDto } from './dto/verify-profile.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -10,6 +11,11 @@ export class AuthController {
     @Post('register')
     async register(@Body() body: RegisterUserDto): Promise<Observable<{ message: string }>> {
         return await this.authService.registerUser(body);
+    }
+
+    @Post('verify-user')
+    async login(@Body() body: VerifyDto): Promise<Observable<{ message: string }>> {
+        return await this.authService.verifyUser(body);
     }
 
     // @Post('login')
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@ import { Inject } from '@nestjs/common';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { Observable, catchError, firstValueFrom, throwError } from 'rxjs';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { VerifyDto } from './dto/verify-profile.dto';
 
 export class AuthService {
     constructor(@Inject('USER_SERVICE') private userServiceClient: ClientProxy) {}
@@ -10,6 +11,14 @@ export class AuthService {
         return await firstValueFrom(
             this.userServiceClient
                 .send({ cmd: 'register' }, data)
+                .pipe(catchError((error) => throwError(() => new RpcException(error.response)))),
+        );
+    }
+
+    public async verifyUser(data: VerifyDto): Promise<Observable<{ message: string }>> {
+        return await firstValueFrom(
+            this.userServiceClient
+                .send({ cmd: 'verify-user' }, data)
                 .pipe(catchError((error) => throwError(() => new RpcException(error.response)))),
         );
     }
