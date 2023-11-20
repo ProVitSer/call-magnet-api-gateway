8c0c783bbd70e75a6766bbd0afa3f2be028c4852
ProVitSer <prokinvs@gmail.com>
2023-11-20 22:15:05 +0000

    ref: del token in UpdatePasswordDto

--- src/auth/dto/update-password.dto.ts
+++ src/auth/dto/update-password.dto.ts
@@ -3,9 +3,6 @@ import { IsString, MinLength } from 'class-validator';
 
 export class UpdatePasswordDto {
     @IsString()
-    token: string;
-
-    @IsString()
     @MinLength(8, { message: MIN_LENGTH_PASSWORD })
     password: string;
 }
