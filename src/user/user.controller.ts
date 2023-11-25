650257c59406147f1d6bb0474c4781c7ec37d40e
ProVitSer <prokinvs@gmail.com>
2023-11-25 19:42:30 +0000

    feat: add get client info proxy req

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Req, Res, UseGuards } from '@nestjs/common';
 import { UserService } from './user.service';
+import { JwtAuthGuard } from '@app/auth/guards/access-token-auth.guard';
+import { GetCurrentClientId } from '@app/common/decorators/get-current-client-id.decorator';
+import { HttpResponseService } from '@app/http/http.service';
+import { Request, Response } from 'express';
+import { GetClientInfoResponse } from '@app/platform-types/user/interfaces';
 
 @Controller('user')
 export class UserController {
     constructor(private readonly userService: UserService) {}
+
+    @UseGuards(JwtAuthGuard)
+    @Get('client-info')
+    async getClientInfo(@Req() req: Request, @Res() res: Response, @GetCurrentClientId() clientId: string) {
+        const response = await this.userService.getClientInfo(clientId);
+        return HttpResponseService.response<GetClientInfoResponse>(req, res, HttpStatus.OK, response);
+    }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,25 @@
+import { MessagePatternCmd } from '@app/platform-types/client-proxy/types';
 import { Inject, Injectable } from '@nestjs/common';
-import { ClientProxy } from '@nestjs/microservices';
+import { ClientProxy, RpcException } from '@nestjs/microservices';
+import { catchError, firstValueFrom, throwError } from 'rxjs';
 
 @Injectable()
 export class UserService {
     constructor(@Inject('USER_SERVICE') private userServiceClient: ClientProxy) {}
+
+    public async findUserByClientId(clientId: string) {
+        return await firstValueFrom(
+            this.userServiceClient
+                .send({ cmd: MessagePatternCmd.findUserByClientId }, clientId)
+                .pipe(catchError((error) => throwError(() => new RpcException(error.response)))),
+        );
+    }
+
+    public async getClientInfo(clientId: string) {
+        return await firstValueFrom(
+            this.userServiceClient
+                .send({ cmd: MessagePatternCmd.getClientInfo }, clientId)
+                .pipe(catchError((error) => throwError(() => new RpcException(error.response)))),
+        );
+    }
 }
